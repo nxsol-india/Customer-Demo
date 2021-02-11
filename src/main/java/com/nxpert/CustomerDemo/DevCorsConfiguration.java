cfd9acbb9b8f2e2331c8523c1b72e1da0ea5a37d
meena <meena@meena-Vostro-3549>
2021-02-11 12:31:43 +0530

    added swagger

--- src/main/java/com/nxpert/CustomerDemo/DevCorsConfiguration.java
+++ src/main/java/com/nxpert/CustomerDemo/DevCorsConfiguration.java
@@ -11,7 +11,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 @Configuration
 @EnableWebMvc
 public class DevCorsConfiguration implements Filter, WebMvcConfigurer {
@@ -22,6 +25,21 @@ public class DevCorsConfiguration implements Filter, WebMvcConfigurer {
 	        registry.addMapping("/**");
 	    }
  
+	 
+	 @Override
+	 public void addInterceptors(InterceptorRegistry registry) {
+	     registry.addInterceptor(new LocaleChangeInterceptor());
+	 }
+
+	 @Override
+	 public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	     registry.addResourceHandler("swagger-ui.html")
+	       .addResourceLocations("classpath:/META-INF/resources/");
+
+	     registry.addResourceHandler("/webjars/**")
+	       .addResourceLocations("classpath:/META-INF/resources/webjars/");
+	 }
+	 
 	    @Override
 	    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) {
 	      HttpServletResponse response = (HttpServletResponse) res;
