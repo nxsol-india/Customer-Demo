4faecf46b250e9d5cd3319dcf695d7daab1cd511
meena <meena@meena-Vostro-3549>
2021-02-12 14:48:29 +0530

    fix coding in master

--- src/main/java/com/nxpert/CustomerDemo/CustomerDemoApplication.java
+++ src/main/java/com/nxpert/CustomerDemo/CustomerDemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomerDemoApplication {
+public class CustomerDemoApplication { 
     
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerDemoApplication.class, args);
